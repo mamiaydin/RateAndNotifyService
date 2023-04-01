88423d761ddbb3c47b42ab11a22ce8de9b8c3b00
mamiaydin <muha2metaydin@gmail.com>
2023-04-01 03:18:47 +0300

    BackgroundService now try to listen rabbitmq if can
    if can not, try to listen again in 5 seconds periods

--- NotificationService/NotificationService/NotificationBackgroundService.cs
+++ NotificationService/NotificationService/NotificationBackgroundService.cs
@@ -5,52 +5,64 @@ namespace NotificationService;
 
 public class NotificationBackgroundService : IHostedService
 {
-    private readonly NotificationListener _listener;
+    private NotificationListener _listener;
     private readonly IServiceScopeFactory _serviceScopeFactory;
-
+    private readonly CancellationTokenSource _cancellationTokenSource;
 
     public NotificationBackgroundService(IServiceScopeFactory serviceScopeFactory)
     {
         _serviceScopeFactory = serviceScopeFactory;
-        try
-        {
-            _listener = new NotificationListener("notifications_queue");
-        }
-        catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException ex)
-        {
-           
-        }
+        _cancellationTokenSource = new CancellationTokenSource();
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        try
-        {
-            
-            _listener.StartListening(async notification =>
-                {
-                    using (var scope = _serviceScopeFactory.CreateScope())
-                    {
-                        var notificationService = scope.ServiceProvider.GetService<IRatingNotificationService>();
-                        await notificationService.CreateNotificationAsync(notification);
-                    }
-                
-                    Console.WriteLine(notification);
-                }
-            );
-            return Task.CompletedTask;
-        }
-        
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An exception occurred while starting the NotificationBackgroundService: {ex}");
-            return Task.FromException(ex);
-        }
+        StartListening();
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        _listener.Dispose();
+        StopListening();
         return Task.CompletedTask;
+    }
+
+    private void StartListening()
+    {
+        Task.Run(() =>
+        {
+            while (!_cancellationTokenSource.IsCancellationRequested)
+            {
+                try
+                {
+                    _listener = new NotificationListener("notifications_queue");
+                    _listener.StartListening(async notification =>
+                    {
+                        using (var scope = _serviceScopeFactory.CreateScope())
+                        {
+                            var notificationService = scope.ServiceProvider.GetService<IRatingNotificationService>();
+                            await notificationService.CreateNotificationAsync(notification);
+                        }
+                    
+                        Console.WriteLine(notification);
+                    });
+
+                    Console.WriteLine("Listening started");
+                    break;
+                }
+                catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException ex)
+                {
+                    Console.WriteLine("Connection failed, retrying in 5 seconds");
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+            }
+        }, _cancellationTokenSource.Token);
+    }
+
+    private void StopListening()
+    {
+        _cancellationTokenSource.Cancel();
+        _listener.Dispose();
+        Console.WriteLine("Listening stopped");
     }
 }
